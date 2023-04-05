ded99424bcb416329b5cdf080541007118f85358
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-05 09:41:51 -0600

    study for loops

--- UdemyCourse/ForLoops/ForLoops/main.swift
+++ UdemyCourse/ForLoops/ForLoops/main.swift
@@ -1,11 +1,19 @@
-//
-//  main.swift
-//  ForLoops
-//
-//  Created by Aldair Zamora on 5/4/23.
-//
+var myFruitArray = ["bananas", "apple", "orange"]
 
-import Foundation
+// print all the elements in the array, but not one by one
+for fruit in myFruitArray {
+    print(fruit)
+}
 
-print("Hello, World!")
+var myNumbers = [10, 20, 30, 40, 50, 60]
 
+for number in myNumbers {
+    print(number / 5) // apply operation to each number in the array
+}
+
+
+// loop through number sequence
+print("\n")
+for i in 1...5 {
+    print(i)
+}
