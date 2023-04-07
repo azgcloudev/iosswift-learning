d6f5db6c8e58a155d8ce4cd927081d4d21df916f
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-07 09:12:06 -0600

    work with safety and scopes, complete calculator functionalities

--- UdemyCourse/WorstCalculator/WorstCalculator/ViewController.swift
+++ UdemyCourse/WorstCalculator/WorstCalculator/ViewController.swift
@@ -8,45 +8,106 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     // input number textfields
     @IBOutlet weak var firstNumber: UITextField!
     @IBOutlet weak var secondNumber: UITextField!
-    
+
     // label to display the result operation
     @IBOutlet weak var resultLabel: UILabel!
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    
+
+
     @IBAction func multiplicationBtn(_ sender: Any) {
-        
-//        let n1: Int = Int(firstNumber.text)
+
         if let n1 = Int(firstNumber.text!) {
-            
+            if let n2 = Int(secondNumber.text!) {
+
+                let result = n1 * n2
+                resultLabel.text = String(result)
+            } else {
+                let warningOperation: String = "NaN"
+                resultLabel.text = warningOperation
+            }
+        } else {
+            let warningOperation: String = "NaN"
+            resultLabel.text = warningOperation
         }
-        let n2: Int = Int(secondNumber.text!)!
-        
-        let result = n1 * n2
-        
-        resultLabel.text = String(result)
+
+
     }
-    
-    
+
+
     @IBAction func divisionBtn(_ sender: Any) {
+
+        if let n1 = Double(firstNumber.text!) {
+            if let n2 = Double(secondNumber.text!) {
+
+                var result: Double;
+
+                if (n1 == 0 && n2 == 0) {
+                    result = 0;
+                    resultLabel.text = String(format: "%.0f", result)
+                } else {
+                    result = n1 / n2
+                    resultLabel.text = String(format: "%.2f", result)
+                }
+
+            } else {
+                let warningOperation: String = "NaN"
+                resultLabel.text = warningOperation
+            }
+        } else {
+            let warningOperation: String = "NaN"
+            resultLabel.text = warningOperation
+        }
+
+
     }
-    
-    
+
+
     @IBAction func additionBtn(_ sender: Any) {
+
+        if let n1 = Int(firstNumber.text!) {
+            if let n2 = Int(secondNumber.text!) {
+
+                let result = n1 + n2
+                resultLabel.text = String(result)
+            } else {
+                let warningOperation: String = "NaN"
+                resultLabel.text = warningOperation
+            }
+        } else {
+            let warningOperation: String = "NaN"
+            resultLabel.text = warningOperation
+        }
+
     }
-    
-    
+
+
     @IBAction func substractionBtn(_ sender: Any) {
+
+        if let n1 = Int(firstNumber.text!) {
+            if let n2 = Int(secondNumber.text!) {
+
+                let result = n1 - n2
+                resultLabel.text = String(result)
+            } else {
+                let warningOperation: String = "NaN"
+                resultLabel.text = warningOperation
+            }
+        } else {
+            let warningOperation: String = "NaN"
+            resultLabel.text = warningOperation
+        }
+
+
     }
-    
+
 }
 
