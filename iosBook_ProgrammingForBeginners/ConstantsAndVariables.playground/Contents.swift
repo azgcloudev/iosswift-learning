9a5e3db91e37cc6927316f0a91d51b330c17b390
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-09 15:01:00 -0600

    study constants and variables declaration, type inference and type annotation

---
+++ iosBook_ProgrammingForBeginners/ConstantsAndVariables.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// constants
+// values of constants cannot be change once it is declared
+let theAnswerToTheUltimateQuestion = 42
+let pi = 3.14159
+let myName = "Aldair Zamora" // string literal
+let isRaining = false
+
+
+// declaring variables
+var currentTemperatureInCelsius = 27
+var myAge = 50
+var myLocation = "home"
+
+
+// type inference, swift automatically detects the variable/constant type based on the value
+
+
+
+// type annotation
+var restaurantRating: Double = 3
