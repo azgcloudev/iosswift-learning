8c82e3595bf07d34dc2d02c13e1a57cb43cdd952
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-09 14:13:50 -0600

    learning data simple data types

---
+++ iosBook_ProgrammingForBeginners/DataTypes.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// INTEGERS 'Int'
+// no decimals, include negative numbers
+42
+-23
+
+// FLOTING POINT
+// numbers with decimals
+// Doubles use 64 bits (default in swift) 'Double'
+// Floats uses 32 bits 'Float'
+3.14159
+0.1
+
+
+
+// BOOLEANS
+// yes or no AKA true or false
+// 'Bool'
+true
+false
+
+
+// STRINGS
+// sequence of characters 'String'
+// unicode complaint
+"hello, world"
+"albatross"
+
