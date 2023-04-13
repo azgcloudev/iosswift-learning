ff24a7e6eaa42a501fc54786112f801d08d099e7
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-13 10:02:09 -0600

    added table view, data source and delegate

--- YoutubeCourse/RandomColors/RandomColors/ColorsTableVC.swift
+++ YoutubeCourse/RandomColors/RandomColors/ColorsTableVC.swift
@@ -9,26 +9,30 @@ import UIKit
 
 class ColorsTableVC: UIViewController {
 
-    @IBAction func temoButtonSegue(_ sender: Any) {
-        
-        performSegue(withIdentifier: "toColorsDetailVC", sender: nil)
-    }
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
+
+}
+
+
+// table view
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toColorsDetailVC", sender: nil)
+    }
 }
