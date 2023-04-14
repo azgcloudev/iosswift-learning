82c6dd407d33383c6f7c6327a89e53d5ab3a4255
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-14 10:04:41 -0600

    update

--- appleTutorialsUIKit/Today/Today/ReminderListViewController.swift
+++ appleTutorialsUIKit/Today/Today/ReminderListViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ReminderListViewController: UIViewController {
+class ReminderListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+        }
     }
 
 
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
 }
 
