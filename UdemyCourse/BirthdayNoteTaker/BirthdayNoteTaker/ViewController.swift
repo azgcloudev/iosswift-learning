ad9466d6f3dcb2f045791190341c6f52237fa6b3
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-14 10:28:17 -0600

    added delete data from user default storage

--- UdemyCourse/BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
+++ UdemyCourse/BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
@@ -10,42 +10,60 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
-    
+
     @IBOutlet weak var birthdayTextField: UITextField!
-    
+
     @IBOutlet weak var nameLabel: UILabel!
-    
+
     @IBOutlet weak var birthdayLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+
         let storedName = UserDefaults.standard.object(forKey: "name")
         let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
-        
+
         // casting -  as? vs as!
         // nameLabel.text = storedName as? String
         // nameLabel.text = storedName as! String // here if the cast fail, the app crash
-            
+
         if let myName = storedName as? String {
-            nameLabel.text = myName
+            nameLabel.text = "Name: \(myName)"
         }
-        
+
         if let newBirthday = storedBirthday as? String {
-            birthdayLabel.text = newBirthday
+            birthdayLabel.text = "Birthday: \(newBirthday)"
         }
     }
 
-    
+
     @IBAction func saveTap(_ sender: Any) {
-        
+
         UserDefaults.standard.set(nameTextField.text!, forKey: "name")
         UserDefaults.standard.set(birthdayTextField.text!, forKey: "birthday")
-        
+
         nameLabel.text = "Name: \(nameTextField.text ?? "")"
         birthdayLabel.text = "Birthday: \(birthdayTextField.text ?? "")"
     }
-    
+
+    @IBAction func deleteTapped(_ sender: Any) {
+
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+
+
+
+        if (storedName as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name:"
+        }
+
+        if (storedBirthday as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text = "Birthday:"
+        }
+
+    }
 }
 
