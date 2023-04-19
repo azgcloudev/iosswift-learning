b0d2aa7fb5f9bf4f216fca9515fde174273d6926
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-19 08:25:46 -0600

    review sets

---
+++ 100DaysOfSwift/Day002/Sets.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// create a set based on an array
+// sets are unoredered and stored randomly
+// cannot have duplicate values
+
+let mySet = Set(["red", "green", "blue"])
+
+let noDuplicates = Set(["red", "red", "green", "blue", "red", "blue", "orange"])
