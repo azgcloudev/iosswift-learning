670a30596970a7822c57c683f173362b0aa74293
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-19 08:34:28 -0600

    review tuples

---
+++ 100DaysOfSwift/Day002/Tuples.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// sets are key values with fixed size
+// can modify the value but keeping the same type
+
+let fullName = (firstName: "Aldair", lastName: "Zamora")
+
+// can access values with index or key
+fullName.0
+fullName.firstName
