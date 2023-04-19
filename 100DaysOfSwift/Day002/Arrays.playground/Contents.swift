f6244af1554c6502bf76c51bc32aedc0ee76bc02
Aldair Zamora <aldair.zamora@outlook.com>
2023-04-19 08:15:01 -0600

    review of arrays

---
+++ 100DaysOfSwift/Day002/Arrays.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// creating an array
+let metallica: [String] = ["James Hetfield", "Lars Ulrich", "Kirk Hammet", "Robert Trujillo"]
+
+// access an array member by index
+metallica[3]
+
+
+// if an index doesn't exist it will thrown an error
