40757dfb17dd5f38514c2b76f9fa7005990e7e8b
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-20 23:36:01 -0600

    complete arithmetic operators on day 3

--- 100DaysOfSwift/Day003/ArithmeticOperators.playground/Contents.swift
+++ 100DaysOfSwift/Day003/ArithmeticOperators.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-let firstScore = 14
-let secondScore = 13
+let firstScore = 12
+let secondScore = 4
 
 // sum
 let result = firstScore + secondScore
@@ -16,3 +16,25 @@ let result3 = result2 * firstScore
 let result4 = result3 / result2
 
 
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+let product = firstScore * secondScore
+let devided = firstScore / secondScore
+let remainder = 13 % secondScore
+
+
+
+// modulo operator example
+let weeks = 465 / 7
+let days = 465 % 7
+print("There are \(weeks) weeks and \(days) days until the event.")
+
+// special function to see if a number is divided equally to another number
+let number = 465
+let isMultiple = number.isMultiple(of: 7)
+print("")
+
+print("=> Is number multiple of a number")
+print("\(number) is multiple of 7: \(isMultiple)")
+print("")
