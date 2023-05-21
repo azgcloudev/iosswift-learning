04cc475b797cb0094ea90069c10ea3a52c3a1009
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-20 23:51:01 -0600

    review operator overloading

--- 100DaysOfSwift/Day003/OperatorOverloading.playground/Contents.swift
+++ 100DaysOfSwift/Day003/OperatorOverloading.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// use + with numbers and strings and arrays
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+// join strings
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+// join arrays
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
