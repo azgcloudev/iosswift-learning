a0132726f2503b2876b00028718cb9d3d16f41d1
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-27 14:58:39 -0600

    programatically setup

---
+++ YoutubeCourse/CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Aldair Zamora on 27/5/23.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        // Do any additional setup after loading the view.
+    }
+
+}
