e25edd24c3aa21d7a36fd27e1912f5def1ae00fd
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-27 09:40:49 -0600

    started logic set up

--- YoutubeCourse/CardWorkout/CardWorkout/CardSelectionVC.swift
+++ YoutubeCourse/CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    @IBOutlet var cardImageView: UIImageView!
+//    @IBOutlet var stopButton: UIButton!
+//    @IBOutlet var restartButton: UIButton!
+//    @IBOutlet var rulesButton: UIButton!
+    @IBOutlet var buttons: [UIButton]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        stopButton.layer.cornerRadius = 8
+//        restartButton.layer.cornerRadius = 8
+//        rulesButton.layer.cornerRadius = 8
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
     }
 
+    @IBAction func stopButtonTapped(_ sender: Any) {
+    }
+
+
+    @IBAction func restartButtonTapped(_ sender: Any) {
+    }
+
+    @IBAction func rulesButtonTapped(_ sender: Any) {
+    }
 
 }
 
