f15287873bab26a4dc0703341a4daf7c6ac1ed8d
Aldair Zamora <aldair.zamora@outlook.com>
2023-05-27 09:07:12 -0600

    first commit, created layout

---
+++ YoutubeCourse/CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardWorkout
+//
+//  Created by Aldair Zamora on 27/5/23.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
